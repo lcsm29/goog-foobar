96a4e8028773fd67938581ac40a5fa25b8ceb2f7
lcsm29 <lcsm29@outlook.com>
2021-09-16 21:03:29 +0900

    Update Level 1: I Love Lance & Janice

--- level1/minion_task_scheduling/solution.py
+++ level1/minion_task_scheduling/solution.py
@@ -1,3 +1,7 @@
-def solution(data, n): 
+def solution(data, n):  # the one that I submitted on Invitation #A3
     freq = {num: data.count(num) for num in data}
     return [num for num in data if freq[num] <= n]
+
+
+def solution(data, n):  # the one that I submitted on Invitation #A4
+    return [i for i in data if data.count(i) <= n]
